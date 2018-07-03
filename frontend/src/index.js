cdb8b25a25fc1dede1c8a67ef4ac59eb4fe2b67f
Helen Liutongco <32890150+hliutongco@users.noreply.github.com>
2018-07-03 11:19:43 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,5 @@
-// You can edit/delete the below code to your liking
-
 document.addEventListener("DOMContentLoaded", function() {
 
-  const noteList = document.getElementById('note-list')
-  const mainDiv = document.getElementById('main')
 
 
 });
